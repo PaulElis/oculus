9347fc4fad2d8dee5cba566f68a119d154024777
github username <elis.paul7@gmail.com>
2018-09-05 20:49:32 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        <header className="app-header">
+          Hi
+        </header>
+
+      </div>
+    );
+  }
+}
+
+export default App;
